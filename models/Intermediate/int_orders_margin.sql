e511677b52c5975c6463f34ab27d97536bffe6b5
j.santmil@gmail.com <j.santmil@gmail.com>
2024-02-06 14:05:36 +0000

    commit margin and cost

---
+++ models/Intermediate/int_orders_margin.sql
@@ -0,0 +1,22 @@
+with 
+source_sales as (
+    select * from {{ source('raw', 'sales') }}
+),
+source_product as (
+    select 
+        *,
+        SAFE_CAST(purchse_price AS FLOAT64) AS purchse_price_float64
+    from {{ source('raw', 'product') }}
+)
+select
+    s.date_date,
+    s.orders_id,
+    s.pdt_id as product_id,
+    s.revenue,
+    s.quantity,
+    p.purchse_price_float64 as purchse_price,
+    ROUND((s.quantity * p.purchse_price_float64),2) AS purchase_cost,
+    ROUND((s.revenue - (s.quantity * p.purchse_price_float64)),2) AS margin
+from source_sales s
+JOIN source_product p
+ON s.pdt_id = p.products_id
