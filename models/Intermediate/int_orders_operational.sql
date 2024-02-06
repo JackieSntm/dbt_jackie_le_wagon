a7f488ab43db2a8f912caefafe1e108e613795d6
j.santmil@gmail.com <j.santmil@gmail.com>
2024-02-06 15:37:52 +0000

    finance MART model

--- models/Intermediate/int_orders_operational.sql
+++ models/Intermediate/int_orders_operational.sql
@@ -5,11 +5,12 @@ WITH op_costs AS (
         CAST(logCost AS FLOAT64) AS logCost_numeric,
         CAST(ship_cost AS FLOAT64) AS ship_cost_numeric
     FROM {{ ref('int_orders_margin') }} AS ma
-    JOIN {{ ref('stg_raw__ship') }} USING (orders_id)
-)
+    JOIN {{ ref('stg_raw__ship') }} USING (orders_id))
 SELECT
     op_costs.orders_id,
     op_costs.date_date,
-    ROUND((margin_product - logCost_numeric - ship_cost_numeric + shipping_fee_numeric), 2) AS operational_margin,
+    op_costs.revenue_product,
+    op_costs.margin_product,
+    ROUND(margin_product - logCost_numeric - ship_cost_numeric + shipping_fee_numeric, 2) AS operational_margin,
     qty_product
 FROM op_costs
