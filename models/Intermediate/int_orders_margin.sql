4bbe715de68195980790dff99a3c3e874abb42d1
j.santmil@gmail.com <j.santmil@gmail.com>
2024-02-06 15:04:04 +0000

    operation margin

--- models/Intermediate/int_orders_margin.sql
+++ models/Intermediate/int_orders_margin.sql
@@ -1,27 +1,3 @@
-WITH int_sales_margin AS (
-with 
-source_sales as (
-    select * from {{ source('raw', 'sales') }}
-),
-source_product as (
-    select 
-        *,
-        SAFE_CAST(purchse_price AS FLOAT64) AS purchse_price_float64
-    from {{ source('raw', 'product') }}
-)
-select
-    s.date_date,
-    s.orders_id,
-    s.pdt_id as products_id,
-    s.revenue,
-    s.quantity,
-    p.purchse_price_float64 as purchse_price,
-    ROUND((s.quantity * p.purchse_price_float64),2) AS purchase_cost,
-    ROUND((s.revenue - (s.quantity * p.purchse_price_float64)),2) AS margin
-from source_sales s
-JOIN source_product p
-ON s.pdt_id = p.products_id
-)
 SELECT 
 orders_id
 , date_date
@@ -29,6 +5,5 @@ orders_id
 , ROUND(SUM(quantity),2) AS qty_product
 , ROUND(SUM(purchase_cost),2) AS purchase_cost_product
 , ROUND(SUM(margin),2) AS margin_product
-FROM int_sales_margin
+FROM {{ ref('int_sales_margin') }}
 GROUP BY orders_id, date_date
-
---
+++ models/Intermediate/int_orders_operational.sql
@@ -0,0 +1,15 @@
+WITH op_costs AS (
+    SELECT 
+        ma.*,
+        CAST(shipping_fee AS FLOAT64) AS shipping_fee_numeric,
+        CAST(logCost AS FLOAT64) AS logCost_numeric,
+        CAST(ship_cost AS FLOAT64) AS ship_cost_numeric
+    FROM {{ ref('int_orders_margin') }} AS ma
+    JOIN {{ ref('stg_raw__ship') }} USING (orders_id)
+)
+SELECT
+    op_costs.orders_id,
+    op_costs.date_date,
+    ROUND((margin_product - logCost_numeric - ship_cost_numeric + shipping_fee_numeric), 2) AS operational_margin,
+    qty_product
+FROM op_costs
