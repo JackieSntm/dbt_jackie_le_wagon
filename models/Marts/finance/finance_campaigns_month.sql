d287e6c80ce62a9d4ea8b0f2f18672a68e7c145d
j.santmil@gmail.com <j.santmil@gmail.com>
2024-02-07 13:14:37 +0000

    finance by month

---
+++ models/Marts/finance/finance_campaigns_month.sql
@@ -0,0 +1,15 @@
+WITH month_group AS(
+SELECT
+*,
+CONCAT(EXTRACT(YEAR FROM date_date), "-", EXTRACT(MONTH FROM date_date),"-", "01") AS date_month
+FROM{{ ref('finance_campaigns_day') }}
+)
+SELECT
+
+date_month
+, SUM(ads_margin) AS ads_margin
+, SUM(average_basket) AS average_basket
+, SUM(operational_margin) AS operational_margin
+, sum(ads_click) AS ads_click
+FROM month_group
+GROUP BY date_month
